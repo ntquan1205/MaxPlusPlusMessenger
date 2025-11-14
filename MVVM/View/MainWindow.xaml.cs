3de15157f696207c3faaf249cd18afa76f8c3075
KYAN <tvquan12345@gmail.com>
2025-11-15 02:04:53 +1000

    Adding PacketBuilder

--- MVVM/View/MainWindow.xaml.cs
+++ MVVM/View/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace MaxPlusPlusMessenger
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
--- Net/Server.cs
+++ Net/Server.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChatClient.Net.IO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
@@ -10,17 +11,20 @@ namespace ChatClient.Net
     class Server
     {
         TcpClient _client;
-
         public Server() 
         {
             _client = new TcpClient();
         }
 
-        public void ConnectToServer() 
+        public void ConnectToServer(string username) 
         {
             if (!_client.Connected) 
             {
                 _client.Connect("127.0.0.1", 7891);
+                var connectPacket = new PacketBuilder();
+                connectPacket.WriteOpCode(0);
+                connectPacket.WriteString(username);
+                _client.Client.Send(connectPacket.GetPacketBytes());
             }
         }
     }
