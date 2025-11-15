93a9f2fcc5b1781144e8308620ba76042ae399a5
KYAN <tvquan12345@gmail.com>
2025-11-15 12:05:50 +1000

    Adding UserList

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -1,24 +1,47 @@
 ﻿using ChatClient.MVVM.Core;
+using ChatClient.MVVM.Model;
 using ChatClient.Net;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ChatClient.MVVM.ViewModel
 {
     public class MainViewModel
     {
-        public RelayCommand ConnectToServerCommand { get; set; }
 
+        public ObservableCollection<UserModel> Users { get; set; }
+        public RelayCommand ConnectToServerCommand { get; set; }
         public string Username { get; set; }
 
         private Server _server;
         public MainViewModel() 
         {
+            Users = new ObservableCollection<UserModel>();
             _server = new Server();
+            _server.connectedEvent += UserConnected;
             ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer(Username), o => !string.IsNullOrEmpty(Username));
+        }
+
+        private void UserConnected()
+        {
+            var user = new UserModel
+            {
+                UserName = _server.PacketReader.ReadMssage(),
+                UID = _server.PacketReader.ReadMssage(),
+            };
+
+            if (!Users.Any(x => x.UID == user.UID)) 
+            {
+                Application.Current.Dispatcher.Invoke(() => Users.Add(user));
+            }
+            
+
+            
         }
     }
 }
